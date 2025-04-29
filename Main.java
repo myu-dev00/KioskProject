3ce29c520b9354b51ff0ff7e175aab89a1735ace
myu <myu.dev00@gmail.com>
2025-04-29 14:59:21 +0900

    try-catch문 사용한 예외 처리
    isEmpty사용시 ""하면 while의 isEmpty로 인하여 무한 반복 (트라이캐치 구문이 안나타남) 
    
    item == null 사용시 "" 하면 다른 정수외 문자들과 마찬가지로 정수가 아니여서 예외처리 당함

--- Main.java
+++ Main.java
@@ -9,20 +9,29 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         List<MenuItem> menuItems = new ArrayList<>();
         boolean start = true;
+        int intItem;
         menuItems.add(new MenuItem("ShackBurger", "W 6.9", "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("SmokeShack", "W 8.9", "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("Cheeseburger", "W 6.9", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("Hamburger", "W 5.4", "비프패티를 기반으로 야채가 들어간 기본버거"));
 
         while (start) {
-            String item = "";
+            String item = null;
 
-            while (item.isEmpty()) { // 여쭤 볼것- null과 ""중에 어느게 더좋을까요?
+            while (item == null) { // 여쭤 볼것- null과 ""중에 어느게 더좋을까요?
                 for (MenuItem menuItem : menuItems) {
                     System.out.printf("%-15s  |  %-4s  |  %s\n", menuItem.getName(), menuItem.getPrice(), menuItem.getExplain());
                 }
                 System.out.printf("%-14s  |  종료\n", "종료");
                 item = sc.nextLine();
+            }
+            //숫자 외 다른 것 입력시 NumberFormatException 발생
+            //try-catch문으로 예외처리 완료
+            try {
+                intItem = Integer.parseInt(item);
+            } catch (NumberFormatException e){
+                System.out.println("잘못입력하셨습니다. 메뉴에 맞는 번호를 입력 부탁드립니다.");
+                continue;
             }
 
             int itemNum = Integer.parseInt(item);
